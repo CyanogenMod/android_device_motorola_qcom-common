f7548164f5fe8a9f6516fd879ef22471dc8d9331
dhacker29 <davidhackerdvm@gmail.com>
2013-12-16 16:49:33 -0600

    Move wpa_supplicant_overlay to family
    Change-Id: I3d2d50af6866fe30eb0164a1cdbef7387cd6096d

--- Android.mk
+++ Android.mk
@@ -49,14 +49,6 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/hostapd
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wpa_supplicant_overlay.conf
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES := config/$(LOCAL_MODULE)
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/wifi
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := p2p_supplicant_overlay.conf
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
