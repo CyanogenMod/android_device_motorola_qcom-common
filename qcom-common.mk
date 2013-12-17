0cc04332508d71cdf9b2c0f6152cc39f1fc0769a
dhacker29 <davidhackerdvm@gmail.com>
2013-12-16 19:44:46 -0600

    Enable smooth-streaming video-playback
    Change-Id: I4c992a49bd3197797f6436d65972b0fb4615ee7f

--- qcom-common.mk
+++ qcom-common.mk
@@ -152,7 +152,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.composition.type=dyn \
     persist.hwc.mdpcomp.enable=true \
     debug.mdpcomp.logs=0 \
-    debug.enabletr=0
+    debug.enabletr=0 \
+    mm.enable.smoothstreaming=true
 
 # QCOM Display
 PRODUCT_PACKAGES += \
