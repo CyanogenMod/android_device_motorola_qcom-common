17baa8e416bc170e1b815886cf2692ac790f0360
dhacker29 <dhackerdvm@gmail.com>
2014-01-31 11:46:11 -0600

    Add vanquish unified device to build barriers
    Change-Id: Id5fd19380b4163ba6d6133c9878e22b686bea9f4

--- modules/camerawrapper/Android.mk
+++ modules/camerawrapper/Android.mk
@@ -1,4 +1,4 @@
-ifneq ($(filter mb886 xt897 xt897c xt901 xt907 xt925 xt926,$(TARGET_DEVICE)),)
+ifneq ($(filter mb886 vanquish xt897 xt897c xt901 xt907 xt925 xt926,$(TARGET_DEVICE)),)
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
