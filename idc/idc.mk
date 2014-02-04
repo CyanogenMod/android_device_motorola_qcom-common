b34b80ea903163da9004967b15be5e24cfa65fed
dhacker29 <dhackerdvm@gmail.com>
2014-02-03 18:20:31 -0600

    Move all the idc/kl/kc stuff to common, it will use what is needed
    Change-Id: I3500cecbe7f39a8eb892336b93f506ffe525e100

--- idc/idc.mk
+++ idc/idc.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := device/motorola/qcom-common
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/atmxt-i2c.idc:system/usr/idc/atmxt-i2c.idc \
     $(LOCAL_PATH)/idc/melfas-ts.idc:system/usr/idc/melfas-ts.idc \
     $(LOCAL_PATH)/idc/evfwd.idc:system/usr/idc/evfwd.idc \
-    $(LOCAL_PATH)/idc/gpio-keys.idc:system/usr/idc/gpio-keys.idc
+    $(LOCAL_PATH)/idc/gpio-keys.idc:system/usr/idc/gpio-keys.idc \
+    $(LOCAL_PATH)/idc/keypad_8960.idc:system/usr/idc/keypad_8960.idc
