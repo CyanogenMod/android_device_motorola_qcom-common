e12461a380d3cc69d5f32fdd60db93fe556b8342
dhacker29 <davidhackerdvm@gmail.com>
2014-02-06 23:04:46 -0600

    Fix extract-files for unified devices
    Change-Id: I6cbe3847dbeb8a2bdfceceb0c2fc4bb19155fbdf

--- extract-files.sh
+++ extract-files.sh
@@ -7,7 +7,6 @@ fi
 
 test -z "$DEVICE" && echo device not set && exit 2
 test -z "$VENDOR" && echo vendor not set && exit 2
-test -z "$FAMILY" && echo vendor not set && exit 2
 test -z "$VENDORDEVICEDIR" && VENDORDEVICEDIR=$DEVICE
 export VENDORDEVICEDIR
 
@@ -75,38 +74,6 @@ for FILE in `egrep -v '(^#|^$)' ../qcom-common/proprietary-files.txt`; do
     fi
 done
 fi
-
-BASE=../../../vendor/$VENDOR/$FAMILY/proprietary
-rm -rf $BASE/*
-for FILE in `egrep -v '(^#|^$)' ../$FAMILY/common-proprietary-files.txt`; do
-    echo "Extracting /system/$FILE ..."
-    OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-    FILE=${PARSING_ARRAY[0]}
-    DEST=${PARSING_ARRAY[1]}
-    if [ -z $DEST ]
-    then
-        DEST=$FILE
-    fi
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-    if [ "$COPY_FROM" = "" ]; then
-        adb pull /system/$FILE $BASE/$DEST
-        # if file dot not exist try destination
-        if [ "$?" != "0" ]
-          then
-          adb pull /system/$DEST $BASE/$DEST
-        fi
-    else
-        cp $COPY_FROM/$FILE $BASE/$DEST
-        # if file does not exist try destination
-        if [ "$?" != "0" ]
-            then
-            cp $COPY_FROM/$DEST $BASE/$DEST
-        fi
-    fi
-done
 
 BASE=../../../vendor/$VENDOR/qcom-common/proprietary
 rm -rf $BASE/*
