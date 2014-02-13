d55530e3f73e97250d9ec2c9ee6d6602db42936b
dhacker29 <dhackerdvm@gmail.com>
2014-02-13 03:55:57 -0600

    Move mm.enable.smoothstreaming up a level
    Change-Id: Ib376a6792b27a607fe74a0574ed6698cda7e8f19

--- qcom-common.mk
+++ qcom-common.mk
@@ -145,8 +145,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.composition.type=dyn \
     persist.hwc.mdpcomp.enable=true \
     debug.mdpcomp.logs=0 \
-    debug.enabletr=0 \
-    mm.enable.smoothstreaming=true
+    debug.enabletr=0
 
 # QCOM Display
 PRODUCT_PACKAGES += \
