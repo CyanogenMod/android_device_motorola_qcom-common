8b5edee958e581bd2a0ac9cb85fad66e104b59b1
dhacker29 <dhackerdvm@gmail.com>
2014-02-12 20:28:05 -0600

    Revert "Refactor gps HAL to family trees"
    This reverts commit abce624587d6f97bc1823a7463766e1624c93d16.
    
    Change-Id: I742bd0e3e0255d9496e6f7e4ee36fce07b560394

--- qcom-common.mk
+++ qcom-common.mk
@@ -130,6 +130,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/scripts/mount_pds.sh:system/bin/mount_pds.sh
 
+# GPS configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/gps.conf:system/etc/gps.conf
+
 # We have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
