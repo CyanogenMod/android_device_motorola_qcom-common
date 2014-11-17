514221a35306a92361c1b8e4920085cbf1515161
dhacker29 <dhackerdvm@gmail.com>
2014-11-16 21:47:34 -0500

    prop: Disable lpa enable tunnel mode audio
    Change-Id: I4fd43722d464d21ea36dcd65a4b16de9e7635dac

--- qcom-common.mk
+++ qcom-common.mk
@@ -223,7 +223,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-    lpa.decode=true \
+    lpa.decode=false \
+    tunnel.decode=true \
+    tunnel.audiovideo.decode=true \
+    lpa.use-stagefright=true \
     qcom.hw.aac.encoder=true \
     af.resampler.quality=255 \
     persist.audio.lowlatency.rec=false
