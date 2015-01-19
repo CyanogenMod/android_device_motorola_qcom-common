062797075a72886e5d7315994356766e1f65d770
Emerson Pinter <dev@pinter.com.br>
2015-01-19 17:56:33 -0200

    Add XT925 to build barriers
    Change-Id: I98b18db70813b45097e3e8c8f6cd9aaeabe2024b

--- modules/batt_health/Android.mk
+++ modules/batt_health/Android.mk
@@ -1,4 +1,4 @@
-ifneq ($(filter mb886 moto_msm8960_jbbl vanquish xt897 xt897c,$(TARGET_DEVICE)),)
+ifneq ($(filter mb886 moto_msm8960_jbbl vanquish xt897 xt897c xt925_jbbl,$(TARGET_DEVICE)),)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
