a1afe74684fc4b7eb620952bd6be5a8e564f01fc
nadlabak <pavel@doshaska.net>
2015-10-20 23:32:11 +0200

    qcom-common: Build BoringSSL compat wrapper
    Must be LD_PRELOADed in device specific init*rc
    
    Change-Id: I1ca7c014c2fa461146cf9e13a6615d002959a3b7

--- qcom-common.mk
+++ qcom-common.mk
@@ -86,6 +86,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libstlport
 
+# BoringSSL compat symbols wrapper
+PRODUCT_PACKAGES += \
+    libboringssl-compat
+
 # Qcom SoftAP
 PRODUCT_PACKAGES += \
     libQWiFiSoftApCfg
