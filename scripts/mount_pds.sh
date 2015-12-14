780893b1d0b25b88324212313a08702e0a59f0f7
nadlabak <pavel@doshaska.net>
2015-12-14 23:29:38 +0100

    Fix binary paths in pds_mount.sh (toybox time)
    Change-Id: Ife9bde8b951ff8dcd6545a84a9e7db90ab3eb861

--- scripts/mount_pds.sh
+++ scripts/mount_pds.sh
@@ -4,11 +4,11 @@ export PATH=/system/xbin:$PATH
 if [ ! -f /cache/pds-CM.img ]
 then
     #make a copy of pds in /cache
-    /system/xbin/dd if=/dev/block/platform/msm_sdcc.1/by-name/pds of=/cache/pds-CM.img
+    /system/bin/dd if=/dev/block/platform/msm_sdcc.1/by-name/pds of=/cache/pds-CM.img
     echo "Backed up PDS"
 fi
 
 #mount the fake pds
-/system/xbin/losetup /dev/block/loop0 /cache/pds-CM.img
+/system/bin/losetup /dev/block/loop0 /cache/pds-CM.img
 /system/bin/mount -o rw -t ext3 /dev/block/loop0 /pds
 echo "Mounted PDS"
